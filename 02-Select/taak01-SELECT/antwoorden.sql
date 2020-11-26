bcb4c515516cd05a0f707e116576ae41390d23f8
Baran <barandemir3322@outlook.com>
2020-11-26 10:55:26 +0100

    02-select taak01 5 / 7 af

--- 02-Select/taak01-SELECT/antwoorden.sql
+++ 02-Select/taak01-SELECT/antwoorden.sql
@@ -1,13 +1,13 @@
--- Opdracht 1 - voorbeeld
-SELECT * FROM jaar2016
--- Opdracht 2 
+-- Opdracht 1 - SELECT * FROM `jaar2015` WHERE 7
 
--- Opdracht 3
+-- Opdracht 2 SELECT * FROM jaar2015 WHERE `rank`=7
 
--- Opdracht 4
+-- Opdracht 3 SELECT * FROM jaar2015 WHERE `score`=7
 
--- Opdracht 5
+-- Opdracht 4 SELECT * FROM jaar2016 WHERE `score`=7
 
--- Opdracht 6
+-- Opdracht 5 SELECT * FROM jaar2015 WHERE region="nord america" 
+
+-- Opdracht 6  
 
 -- Opdracht 7
